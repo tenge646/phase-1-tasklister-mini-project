4f78f0db00c2a7709c353c348360c13f3cd5b20f
kinsman646 <teesam2004@gmail.com>
2023-09-29 12:26:44 +0300

    Completed assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,32 @@
-document.addEventListener("DOMContentLoaded", () => {
+
   // your code here
-});
+  document.addEventListener("DOMContentLoaded", function() {
+    const form = document.getElementById("create-task-form");
+    const taskList = document.getElementById("tasks");
+    
+    form.addEventListener("submit", function(event) {
+      event.preventDefault(); 
+      const taskDescriptionInput = document.getElementById("new-task-description");
+      const taskDescription = taskDescriptionInput.value;
+      const taskItem = document.createElement("li");
+      
+      taskItem.textContent = taskDescription;
+      
+      const deleteButton = document.createElement("button");
+      
+      deleteButton.textContent = "Delete";
+      deleteButton.addEventListener("click", function() {
+        taskItem.remove(); 
+      });
+     
+      taskItem.appendChild(deleteButton);
+      taskList.appendChild(taskItem);
+      taskDescriptionInput.value = "";
+      taskDescriptionInput.focus();
+    });
+  });
+  
+
+
+
+
